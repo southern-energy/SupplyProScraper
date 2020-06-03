d47e3bcbb20111e7d50096e0d6492ced1d575812
Gregory Power <gregorywpower@gmail.com>
2020-06-03 18:57:03 -0400

    add datetime to differences sheet text

--- Excel_Compare_Tool.py
+++ Excel_Compare_Tool.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
+previous_excel_sheet = str("2020-5-26-11_SPS_All_Builder_Tasks.xlsx")
 
-previous_excel_sheet = str("OldSupplyProOutput.xlsx")
-
-todays_excel_sheet = str("05-15-2020_Scraper_Output.xlsx")
+todays_excel_sheet = str("2020-06-03-18_SPS_All_Builder_Tasks.xlsx")
 
 old_excel_dataframe = pd.read_excel(previous_excel_sheet, sheetname=0, index_col=0, dtype=str)
 
@@ -18,4 +18,10 @@ only_new_entries_dataframe = large_dataframe.drop_duplicates(keep=False)
 
 print(only_new_entries_dataframe)
 
-only_new_entries_dataframe.to_excel("Differences_Sheet.xlsx", index=False)
+print("Current Month, Date & Time:" + str(datetime.now().month)+"-"+str(datetime.now().day)+str(datetime.now().hour))
+
+now = str(str(datetime.today().strftime('%Y-%m-%d'))+"-"+str(datetime.now().hour))
+
+only_new_entries_dataframe.to_excel(str(now) +"_Differences_Sheet.xlsx", index=False)
+
+# only_new_entries_dataframe.to_excel("Differences_Sheet.xlsx", index=False)
