8a695f1e29e06aee1183830297500956c96d5013
Gregory Power <gregorywpower@gmail.com>
2020-10-30 16:47:52 -0400

    create TODOs for new function

--- Hyphen_Solutions_Scraper.py
+++ Hyphen_Solutions_Scraper.py
@@ -4,33 +4,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.webdriver import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
-import bs4
+from bs4 import BeautifulSoup
 import re
 import json
-import numpy as np
 import pandas as pd
 from datetime import datetime
 
 """
-This was the original method I was using when developing this script, please run this if you are curious of what is happening under the hood of Selenium or you need to troubleshoot any issues.
+This was the original method I was using when developing this script, please
+run this if you are curious of what is happening under the hood of Selenium or
+you need to troubleshoot any issues.
 """
-# print("Real Browser Launching")
-# browser = webdriver.Chrome(ChromeDriverManager().install())
-# print("Real Browser has Launched")
+print("Real Browser Launching")
+browser = webdriver.Chrome(ChromeDriverManager().install())
+print("Real Browser has Launched")
 
 """
-The Headless browsing option greatly reduces the amount of time it takes for the scraper to run.
+The Headless browsing option greatly reduces the amount of time it takes for
+the scraper to run.
 """
-print("Headless Browser Running")
-options = Options()
-options.add_argument("--headless") # Runs Chrome in headless mode.
-options.add_argument('--no-sandbox') # Bypass OS security model
-options.add_argument('--disable-gpu')  # applicable to windows os only
-options.add_argument('start-maximized') # 
-options.add_argument('disable-infobars')
-options.add_argument("--disable-extensions")
-browser = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
-print("Headless Browser has Launched")
+# print("Headless Browser Running")
+# options = Options()
+# options.add_argument("--headless") # Runs Chrome in headless mode.
+# options.add_argument('--no-sandbox') # Bypass OS security model
+# options.add_argument('--disable-gpu')  # applicable to windows os only
+# options.add_argument('start-maximized') # 
+# options.add_argument('disable-infobars')
+# options.add_argument("--disable-extensions")
+# browser = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
+# print("Headless Browser has Launched")
 
 # Creating Session
 
@@ -79,10 +81,10 @@ def process_to_get_to_future_orders():
         # print("We are inside future orders.")
         session.get("https://www.hyphensolutions.com/MH2Supply/Reports/PotentialOrders.asp?days="+str(days_from_today)+"&sessid=")
         browser.get("https://www.hyphensolutions.com/MH2Supply/Reports/PotentialOrders.asp?days="+str(days_from_today)+"&sessid=")
-    try: 
-        navigate_to_future_orders(45)
+    try:
+        navigate_to_future_orders(90)
     except:
-        navigate_to_future_orders(45)
+        navigate_to_future_orders(90)
 
     def interact_with_future_orders_page():
         browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[1]/tbody/tr[2]/td[2]/select")
@@ -91,104 +93,6 @@ def process_to_get_to_future_orders():
 process_to_get_to_future_orders()
 
 # ========================================
-
-#TODO: We need to have a way to check which of the two functions we need to implement on any given page.
-
-# def page_or_no_page_checker():
-#     print("We are inside the page checker function.")
-#     if browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[1]/tbody/tr[1]/td[3]/b") == True:
-#         print("The page element exists")
-#     else: 
-#         print("We are working with a one-pager.")
-# page_or_no_page_checker()
-
-# This is the version that will scrape multiple pages. We need a fix for Future Order Pages that are only one page.
-def select_dan_ryan_builders():
-    browser.find_element_by_name("account_id").send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN,Keys.ARROW_DOWN, Keys.RETURN)
-    browser.find_element_by_name("rows_per_page").send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.RETURN)
-    browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[2]/tbody/tr[1]/th[7]/a/span/b").click()
-    Text_For_Counter = browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[1]/tbody/tr[1]/td[3]/b").text
-    print(Text_For_Counter)
-    try:
-        number_of_items = (re.findall(r'^\d\d\d',Text_For_Counter))
-        number_of_items = int(number_of_items[0])
-    except:
-        number_of_items = (re.findall(r'^\d\d',Text_For_Counter))
-        number_of_items = int(number_of_items[0])
-    print(number_of_items)
-    Text_For_Counter = browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[1]/tbody/tr[1]/td[3]/b").text
-    # Variable for how many pages we need to scrape.
-    try:
-        number_of_pages = (re.findall(r'\d\d$',Text_For_Counter))
-        number_of_pages = int(number_of_pages[0])
-    except:
-        number_of_pages = (re.findall(r'\d$',Text_For_Counter))
-        number_of_pages = int(number_of_pages[0])
-
-    print(number_of_pages)
-
-    if number_of_pages == None:
-        tr_elements = browser.find_elements_by_xpath('/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[2]/tbody/tr')
-        i = 0
-        for i in range(1, len(tr_elements)):
-            # print(i)
-            print(tr_elements[i].text, end='\n')
-    else:
-        j = 0
-        i = 0
-        large_dataframe = pd.DataFrame()
-        while j < number_of_pages:
-            # print(i)
-            # print(number_of_pages)
-            html_table = browser.find_element_by_xpath('/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[2]').get_attribute('outerHTML')
-
-            large_dataframe = large_dataframe.append(pd.read_html(html_table, header=0), ignore_index=True)
-
-            browser.find_element_by_xpath('/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[1]/tbody/tr[2]/td[3]/input[3]').send_keys(Keys.RETURN)
-            j += 1
-        else:
-            def export_large_dataframe():
-                print("We are inside the export function.")
-                print(large_dataframe)
-                large_dataframe.to_excel("SupplyProScraperMultipage.xlsx")
-            export_large_dataframe()
-            # def return_to_future_orders_base():
-            #     browser.get("https://www.hyphensolutions.com/MH2Supply/Reports/PotentialOrders.asp?sessid=")
-            # return_to_future_orders_base()
-
-# select_dan_ryan_builders()
-
-# ==============================
-
-def select_dan_ryan_south_carolina():
-    browser.find_element_by_name("account_id").send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.RETURN)
-    browser.find_element_by_name("rows_per_page").send_keys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.RETURN)
-    browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[2]/tbody/tr[1]/th[7]/a/span/b").click()
-    Text_For_Counter = browser.find_element_by_xpath("/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[1]/tbody/tr[2]/td[3]/b").text
-    print(Text_For_Counter)
-    # Variable for how many items we need to scrape.
-    try:
-        number_of_items = (re.findall(r'^\d\d',Text_For_Counter))
-        number_of_items = int(number_of_items[0])
-    except:
-        number_of_items = (re.findall(r'^\d',Text_For_Counter))
-        number_of_items = int(number_of_items[0])
-        
-    print("Number of Items " + str(number_of_items))
-
-    if number_of_items != None:
-        html_table = browser.find_element_by_xpath('/html/body/table[4]/tbody/tr/td[2]/table[2]/tbody/tr/td[2]/form/table[2]').get_attribute('outerHTML')
-        
-        # print(html_table)
-
-        pd.read_html(html_table, header=0)
-        print(pd.read_html(html_table, header=0))
-
-
-# select_dan_ryan_south_carolina()
-
-
-# ===============================
 
 def select_all():
     browser.find_element_by_name("account_id").send_keys(Keys.RETURN)
@@ -243,8 +147,21 @@ def select_all():
 
                 large_dataframe.to_excel(str(now) +"_SPS_All_Builder_Tasks.xlsx", index=False)
             export_large_dataframe()
-            # def return_to_future_orders_base():
-            #     browser.get("https://www.hyphensolutions.com/MH2Supply/Reports/PotentialOrders.asp?sessid=")
-            # return_to_future_orders_base()
 
-select_all()
+# select_all()
+
+# TODO:
+# 1. Grab HTML TABLE
+# 2. Traverse Anchor Links with BeautifulSoup
+# 3. Grab Tasks
+# OR
+# 3. Go to Job Schedule
+# 4. Grab Anchor Links
+# 5. Grab Schedule Table
+# 6. Return PO and total dollar amount
+# 7. Put all that stuff into a table
+# 8. Put that into a History/Archive CSV so we can compare the two sets.
+
+def select_all_jobs():
+    print("We're inside the function")
+select_all_jobs()
