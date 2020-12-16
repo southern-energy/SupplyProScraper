413f44a0a3aaf83fc8ab83a117b67ccd055103db
Gregory Power <gregorywpower@gmail.com>
2020-12-16 10:02:06 -0500

    add method to see old and new data on sheet

--- Excel_Compare_Tool.py
+++ Excel_Compare_Tool.py
@@ -2,21 +2,26 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
-previous_excel_sheet = str("Archive_Sheet.xlsx")
+previous_excel_sheet = str("2020-12-07-16_SPS_All_Builder_Tasks.xlsx")
 
-todays_excel_sheet = str("2020-08-03-12_SPS_All_Builder_Tasks.xlsx")
+todays_excel_sheet = str("2020-12-14-10_SPS_All_Builder_Tasks.xlsx")
 
-old_excel_dataframe = pd.read_excel(previous_excel_sheet, sheet_name=0, index_col=0, dtype=str)
+old_excel_dataframe = pd.read_excel(previous_excel_sheet, sheet_name=0, dtype=str)
+
+old_excel_dataframe["Old_Sheet"] = True
 
 print(old_excel_dataframe)
 
-new_excel_dataframe = pd.read_excel(todays_excel_sheet, sheet_name=0, index_col=0, dtype=str)
+new_excel_dataframe = pd.read_excel(todays_excel_sheet, sheet_name=0, dtype=str)
+
+new_excel_dataframe["Old_Sheet"] = False
 
 large_dataframe = old_excel_dataframe.append(new_excel_dataframe)
 
-only_new_entries_dataframe = large_dataframe.drop_duplicates(keep=False)
+only_new_entries_dataframe = large_dataframe.drop_duplicates(subset=['Account', 'Job', 'Subdivision', 'Lot /Block', 'Task /PO Contract Date','TaxAmount', 'JobSubtotal(Excl Tax)', 'Total'], keep=False)
 
-print(only_new_entries_dataframe)
+
+print(only_new_entries_dataframe.columns)
 
 print("Current Month, Date & Time:" + str(datetime.now().month)+"-"+str(datetime.now().day)+str(datetime.now().hour))
 
